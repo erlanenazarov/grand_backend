e26a66312532c07750c8ffe817be0fb6d117c00d
erlan <admin@abovethesky.ru>
2016-11-02 12:26:38 +0600

    + Fixed more

--- src/GrandChita/AppBundle/Controller/ScheduleController.php
+++ src/GrandChita/AppBundle/Controller/ScheduleController.php
@@ -10,17 +10,9 @@ class ScheduleController extends AbstractController
         $em = $this->getDoctrine()->getManager()->getRepository('GrandChitaEntityBundle:Schedule');
         $items = $em->findAll();
 
-        $finalSchedule = array();
-
-        foreach($items as $item) {
-            if(!$item->getEnabled()) {
-                $finalSchedule[] += $item;
-            }
-        }
-
         return $this->render('GrandChitaAppBundle:Schedule:schedule.html.twig', array(
             'location' => 'schedule',
-            'list' => $finalSchedule
+            'list' => $items
         ));
     }
 
